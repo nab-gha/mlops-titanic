ce048b9f4608806d8aeae6ee401e8621a3916833
paulcarlton-ww <paul.carlton@weave.works>
2020-06-03 16:48:32 +0100

    add  get-port-forward-cmds.sh

--- aws-titanic/get-port-forward-cmds.sh
+++ aws-titanic/get-port-forward-cmds.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "export AWS_ACCESS_KEY_ID=$(jq '."AccessKey"["AccessKeyId"]' $HOME/mlops-user.json)"
 echo "export AWS_SECRET_ACCESS_KEY=$(jq '."AccessKey"["SecretAccessKey"]' $HOME/mlops-user.json)"
-export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION # the region from cloud9 environment
-EKS_CLUSTER_NAME=$EKS_CLUSTER_NAME
-aws eks update-kubeconfig --name $EKS_CLUSTER_NAME
-kubectl port-forward svc/istio-ingressgateway -n istio-system 8080:80 &
+echo "export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" 
+echo "EKS_CLUSTER_NAME=$EKS_CLUSTER_NAME"
+echo "aws eks update-kubeconfig --name $EKS_CLUSTER_NAME"
+echo "kubectl port-forward svc/istio-ingressgateway -n istio-system 8080:80 &"
